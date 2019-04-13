7a736b6b06a351feb76af1349c37f40d23b93cbb
Kjergaard <KjergaardVFX@gmail.com>
2019-04-14 01:16:02 +0200

    Initial commit

---
+++ FacialRecognitionApp/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace FacialRecognitionApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WebcamBox = new System.Windows.Forms.PictureBox();
+            this.IDBox = new System.Windows.Forms.TextBox();
+            this.TrainButton = new System.Windows.Forms.Button();
+            this.OutputBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_ID = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SquareButton = new System.Windows.Forms.Button();
+            this.EyeButton = new System.Windows.Forms.Button();
+            this.PredictButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.WebcamBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WebcamBox
+            // 
+            this.WebcamBox.Location = new System.Drawing.Point(12, 31);
+            this.WebcamBox.Name = "WebcamBox";
+            this.WebcamBox.Size = new System.Drawing.Size(674, 493);
+            this.WebcamBox.TabIndex = 0;
+            this.WebcamBox.TabStop = false;
+            // 
+            // IDBox
+            // 
+            this.IDBox.Location = new System.Drawing.Point(710, 47);
+            this.IDBox.Name = "IDBox";
+            this.IDBox.Size = new System.Drawing.Size(222, 20);
+            this.IDBox.TabIndex = 1;
+            // 
+            // TrainButton
+            // 
+            this.TrainButton.Location = new System.Drawing.Point(710, 76);
+            this.TrainButton.Name = "TrainButton";
+            this.TrainButton.Size = new System.Drawing.Size(222, 34);
+            this.TrainButton.TabIndex = 2;
+            this.TrainButton.Text = "Begin Training";
+            this.TrainButton.UseVisualStyleBackColor = true;
+            this.TrainButton.Click += new System.EventHandler(this.TrainButton_Click);
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Location = new System.Drawing.Point(710, 161);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(222, 213);
+            this.OutputBox.TabIndex = 3;
+            this.OutputBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Webcam";
+            // 
+            // lbl_ID
+            // 
+            this.lbl_ID.AutoSize = true;
+            this.lbl_ID.Location = new System.Drawing.Point(710, 31);
+            this.lbl_ID.Name = "lbl_ID";
+            this.lbl_ID.Size = new System.Drawing.Size(71, 13);
+            this.lbl_ID.TabIndex = 5;
+            this.lbl_ID.Text = "Enter Your ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(710, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Output";
+            // 
+            // SquareButton
+            // 
+            this.SquareButton.Location = new System.Drawing.Point(710, 384);
+            this.SquareButton.Name = "SquareButton";
+            this.SquareButton.Size = new System.Drawing.Size(107, 33);
+            this.SquareButton.TabIndex = 7;
+            this.SquareButton.Text = "Face Squares OFF";
+            this.SquareButton.UseVisualStyleBackColor = true;
+            this.SquareButton.Click += new System.EventHandler(this.SquareButton_Click);
+            // 
+            // EyeButton
+            // 
+            this.EyeButton.Location = new System.Drawing.Point(825, 384);
+            this.EyeButton.Name = "EyeButton";
+            this.EyeButton.Size = new System.Drawing.Size(107, 33);
+            this.EyeButton.TabIndex = 8;
+            this.EyeButton.Text = "Eye Squares OFF";
+            this.EyeButton.UseVisualStyleBackColor = true;
+            this.EyeButton.Click += new System.EventHandler(this.EyeButton_Click);
+            // 
+            // PredictButton
+            // 
+            this.PredictButton.Location = new System.Drawing.Point(710, 462);
+            this.PredictButton.Name = "PredictButton";
+            this.PredictButton.Size = new System.Drawing.Size(222, 33);
+            this.PredictButton.TabIndex = 9;
+            this.PredictButton.Text = "Predict Face";
+            this.PredictButton.UseVisualStyleBackColor = true;
+            this.PredictButton.Click += new System.EventHandler(this.PredictButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 536);
+            this.Controls.Add(this.PredictButton);
+            this.Controls.Add(this.EyeButton);
+            this.Controls.Add(this.SquareButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_ID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.TrainButton);
+            this.Controls.Add(this.IDBox);
+            this.Controls.Add(this.WebcamBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.WebcamBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox WebcamBox;
+        private System.Windows.Forms.TextBox IDBox;
+        private System.Windows.Forms.Button TrainButton;
+        private System.Windows.Forms.RichTextBox OutputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_ID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button SquareButton;
+        private System.Windows.Forms.Button EyeButton;
+        private System.Windows.Forms.Button PredictButton;
+    }
+}
+
---
+++ FacialRecognitionApp/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Face;
+using Emgu.CV.Structure;
+
+namespace FacialRecognitionApp
+{
+    public partial class Form1 : Form
+    {
+        public VideoCapture Webcam { get; set; }
+        public EigenFaceRecognizer FaceRecognition { get; set; }
+        public CascadeClassifier FaceDetection { get; set; }
+        public CascadeClassifier EyeDetection { get; set; }
+
+        public Mat Frame { get; set; }
+
+        public List<Image<Gray, byte>> Faces { get; set; }
+        public List<int> IDs { get; set; }
+
+        public int ProcessedImageWidth { get; set; } = 128;
+        public int ProcessedImageHeight { get; set; } = 150;
+
+        public int TimerCounter { get; set; } = 0;
+        public int TimeLimit { get; set; } = 30;
+        public int ScanCounter { get; set; } = 0;
+
+        public string YMLPath { get; set; } = @"../../Algo/TrainingData.yml";
+
+        public Timer Timer { get; set; }
+
+        public bool FaceSquare { get; set; } = false;
+        public bool EyeSquare { get; set; } = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            FaceRecognition = new EigenFaceRecognizer(80, double.PositiveInfinity);
+            FaceDetection = new CascadeClassifier(System.IO.Path.GetFullPath(@"../../Algo/haarcascade_frontalface_default.xml"));
+            EyeDetection = new CascadeClassifier(System.IO.Path.GetFullPath(@"../../Algo/haarcascade_eye.xml"));
+            Frame = new Mat();
+            Faces = new List<Image<Gray, byte>>();
+            IDs = new List<int>();
+            BeginWebcam();
+        }
+
+        private void BeginWebcam()
+        {
+            if (Webcam == null)
+            {
+                Webcam = new VideoCapture();
+            }
+
+            Webcam.ImageGrabbed += Webcam_ImageGrabbed;
+            Webcam.Start();
+            OutputBox.AppendText($"Webcam Started...{Environment.NewLine}");
+        }
+
+        private void Webcam_ImageGrabbed(object sender, EventArgs e)
+        {
+            Webcam.Retrieve(Frame);
+            var ImageFrame = Frame.ToImage<Bgr, byte>();
+
+            if (ImageFrame != null)
+            {
+                var grayFrame = ImageFrame.Convert<Gray, byte>();
+                var faces = FaceDetection.DetectMultiScale(grayFrame, 1.3, 5);
+                var eyes = EyeDetection.DetectMultiScale(grayFrame, 1.3, 5);
+
+                if (FaceSquare)
+                    foreach (var face in faces)
+                        ImageFrame.Draw(face, new Bgr(Color.BurlyWood), 3);
+                
+
+                if (EyeSquare)
+                    foreach (var eye in eyes)
+                        ImageFrame.Draw(eye, new Bgr(Color.Yellow), 3);
+
+                WebcamBox.Image = ImageFrame.ToBitmap();
+            }
+        }
+
+        private void EyeButton_Click(object sender, EventArgs e)
+        {
+            if (EyeSquare)
+                EyeButton.Text = "Eye Square: Off";
+            else
+                EyeButton.Text = "Eye Square: On";
+
+            FaceSquare = !FaceSquare;
+        }
+
+        private void SquareButton_Click(object sender, EventArgs e)
+        {
+            if (FaceSquare)
+                SquareButton.Text = "Face Square: Off";
+            else
+                SquareButton.Text = "Face Square: On";
+
+            FaceSquare = !FaceSquare;
+        }
+
+        private void PredictButton_Click(object sender, EventArgs e)
+        {
+            Webcam.Retrieve(Frame);
+            var imageFrame = Frame.ToImage<Gray, byte>();
+
+            if (imageFrame != null)
+            {
+                var faces = FaceDetection.DetectMultiScale(imageFrame, 1.3, 5);
+
+                if (faces.Count() != 0)
+                {
+                    var processedImage = imageFrame.Copy(faces[0]).Resize(ProcessedImageWidth, ProcessedImageHeight,
+                        Emgu.CV.CvEnum.Inter.Cubic);
+                    var result = FaceRecognition.Predict(processedImage);
+
+                    if (result.Label.ToString() == "15")
+                        MessageBox.Show("Your Name");
+                    else
+                        MessageBox.Show("Your Friend");
+                }
+                else
+                    MessageBox.Show("Face was not found - try again");
+            }
+            
+        }
+
+        private void TrainButton_Click(object sender, EventArgs e)
+        {
+            if (IDBox.Text != string.Empty)
+            {
+                IDBox.Enabled = !IDBox.Enabled;
+
+                Timer = new Timer();
+                Timer.Interval = 500;
+                Timer.Tick += Timer_Tick;
+                Timer.Start();
+                TrainButton.Enabled = !TrainButton.Enabled;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Webcam.Retrieve(Frame);
+            var imageFrame = Frame.ToImage<Gray, byte>();
+
+            if (TimerCounter < TimeLimit)
+            {
+                TimerCounter++;
+
+                if (imageFrame != null)
+                {
+                    var faces = FaceDetection.DetectMultiScale(imageFrame, 1.3, 5);
+
+                    if (faces.Count() > 0)
+                    {
+                        var processedImage = imageFrame.Copy(faces[0]).Resize(ProcessedImageWidth, ProcessedImageHeight, Emgu.CV.CvEnum.Inter.Cubic);
+                        Faces.Add(processedImage);
+                        IDs.Add(Convert.ToInt32(IDBox.Text));
+                        ScanCounter++;
+                        OutputBox.AppendText($"{ScanCounter} Successful Scans Taken...{Environment.NewLine}");
+                        OutputBox.ScrollToCaret();
+                    }
+                }
+            }
+            else
+            {
+                FaceRecognition.Train(Faces.ToArray(), IDs.ToArray());
+                FaceRecognition.Write(YMLPath);
+                Timer.Stop();
+                TimerCounter = 0;
+                TrainButton.Enabled = !TrainButton.Enabled;
+                IDBox.Enabled = !IDBox.Enabled;
+                OutputBox.AppendText($"Training Complete! {Environment.NewLine}");
+                MessageBox.Show("Training Complete");
+            }
+            
+        }
+    }
+}
